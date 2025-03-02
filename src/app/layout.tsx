1658014dcbb0a13e54a40ed1c9924f6589505e21
Magpys <1dangriffiths@gmail.com>
2025-03-02 20:53:35 +0000

    Stick header and add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body
         className={`antialiased min-h-screen`}
       >
-      <header className={`bg-white flex items-center justify-between p-12 gap-6 h-20`}>
+      <header className={`bg-white flex items-center justify-between p-12 gap-6 h-20 fixed z-10 w-screen`}>
         <Link href={"/"}>
           <Image
             src={`/Capoeira-derby-text-only.png`}
@@ -35,9 +35,25 @@ export default function RootLayout({
           <Link href={"/our-group"}>Our Group</Link>
         </div>
       </header>
-      <main className={"text-gray-900"}>
+      <main className={"text-gray-900 pt-22 bg-green-100 pb-10"}>
         {children}
       </main>
+      <footer className={"flex w-screen bg-amber-50 justify-between p-6 gap-6"}>
+        <div className={"flex flex-col"}>
+          <p>Contact</p>
+          <p>Daniel Griffiths</p>
+          <p>07970 446920</p>
+        </div>
+        <div className={"flex flex-col"}>
+          <Link href={"/what-is-capoeira"}>What is Capoeira?</Link>
+          <Link href={"/can-i-do-capoeira"}>Can I do Capoeira?</Link>
+          <Link href={"/classes"}>Classes</Link>
+          <Link href={"/our-group"}>Our Group</Link>
+        </div>
+        <div>
+          Social media shit!
+        </div>
+      </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
       <section className="relative mx-auto max-w-5xl p-12 flex items-center justify-center">
         <p className={"text-xl font-bold"}>Come and learn the Brazilian martial art of Capoeira, in Derby city.</p>
       </section>
-      <section className={"mx-auto max-w-5xl p-12 flex items-start"}>
+      <section className={"mx-auto max-w-5xl p-6 flex items-center gap-6"}>
         <div className="flex flex-col w-full gap-3 p-6 text-lg">
           <p>
             Capoeira is more than just a martial art—it’s a blend of movement, music, and community.
             Whether you want to improve your fitness, learn acrobatics, or connect with Brazilian culture,
-            our group welcomes all levels and ages. Come train, play, and grow with us!
+            our group welcomes all levels, kids and adults. Come train, play, and grow with us!
           </p>
           <p>
             <Link href={"/classes"}>Join a class today!</Link>
@@ -31,7 +31,7 @@ export default function Home() {
           width={"300"}
           height={"300"} />
       </section>
-      <section className="mx-auto max-w-5xl p-12 flex items-start">
+      <section className="mx-auto max-w-5xl p-6 flex items-center gap-6">
         <Image
           src={"/batteria.jpeg"}
           alt={"A group of musicians playing traditional Capoeira instruments with intent looks of concentration."}
@@ -45,7 +45,14 @@ export default function Home() {
             The ideals of cunning, misdirection and subterfuge run through the art like a lifeblood.
           </p>
           <p>
-            <Link href={"/what-is-capoeira"}>Learn more!</Link>
+            <Link href={"/what-is-capoeira"}>Learn more</Link>
+          </p>
+          <p>
+            We have been teaching Capoeira in Derby city since the group was founded by Mestre Papa-Leguas in 2003,
+            initially under Mestre Adam, and currently under his senior students, Dan, Dave and Andy.
+          </p>
+          <p>
+            <Link href={"/our-group"}>Learn more about our group</Link>
           </p>
         </div>
       </section>
