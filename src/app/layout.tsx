f50dc7bfcf3e441096ac688b82dfa3da961bcf70
Magpys <1dangriffiths@gmail.com>
2025-03-02 20:20:11 +0000

    Add images, add basic layout for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Capoeira Derby",
@@ -25,9 +16,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased min-h-screen`}
       >
+      <header className={`bg-white flex items-center justify-between p-12 gap-6 h-20`}>
+        <Link href={"/"}>
+          <Image
+            src={`/Capoeira-derby-text-only.png`}
+            alt="Capoeira Derby"
+            width={200}
+            height={200}
+            className="object-contain"
+          />
+        </Link>
+        <div className={"flex flex-row justify-center gap-6"}>
+          <Link href={"/what-is-capoeira"}>What is Capoeira?</Link>
+          <Link href={"/can-i-do-capoeira"}>Can I do Capoeira?</Link>
+          <Link href={"/classes"}>Classes</Link>
+          <Link href={"/our-group"}>Our Group</Link>
+        </div>
+      </header>
+      <main className={"text-gray-900"}>
         {children}
+      </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,54 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Link href={"/"} className={"flex gap-6 items-center"}>
-        <Image
-          src={`/Capoeira-derby-header.png`}
-          alt="Capoeira Derby"
-          fill
-        />
-        <div
-          className={`text-4xl text-orange-400 font-bold hidden sm:block`}
-        >
-          Magpys
+    <>
+      <section
+        className="relative w-full h-96 bg-[url('/group-smiling.jpeg')] bg-cover bg-center bg-no-repeat"
+      >
+        <div className="absolute inset-0 bg-black/60 flex items-center justify-center p-6">
+          <h1 className="text-4xl font-bold text-white text-center">Capoeira: Movement, Music, Community</h1>
         </div>
-      </Link>
-      <h1>Welcome to Capoeira Derby!</h1>
-    </div>
+      </section>
+      <section className="relative mx-auto max-w-5xl p-12 flex items-center justify-center">
+        <p className={"text-xl font-bold"}>Come and learn the Brazilian martial art of Capoeira, in Derby city.</p>
+      </section>
+      <section className={"mx-auto max-w-5xl p-12 flex items-start"}>
+        <div className="flex flex-col w-full gap-3 p-6 text-lg">
+          <p>
+            Capoeira is more than just a martial art—it’s a blend of movement, music, and community.
+            Whether you want to improve your fitness, learn acrobatics, or connect with Brazilian culture,
+            our group welcomes all levels and ages. Come train, play, and grow with us!
+          </p>
+          <p>
+            <Link href={"/classes"}>Join a class today!</Link>
+          </p>
+        </div>
+        <Image
+          src={"/adam-kicking.jpeg"}
+          alt={"A man on the ground kicking over the head of a small girl. A group of people watch, playing instruments."}
+          width={"300"}
+          height={"300"} />
+      </section>
+      <section className="mx-auto max-w-5xl p-12 flex items-start">
+        <Image
+          src={"/batteria.jpeg"}
+          alt={"A group of musicians playing traditional Capoeira instruments with intent looks of concentration."}
+          width={"300"}
+          height={"300"}
+        />
+        <div className="flex flex-col w-full gap-3 p-6 text-lg">
+          <p>
+            Developed by African slaves in Brazil, the practice of Capoeira was disguised with ritual and dance
+            so that the slave-owners would remain unaware of it’s <Link href={"/what-is-capoeira"}>true nature</Link>.
+            The ideals of cunning, misdirection and subterfuge run through the art like a lifeblood.
+          </p>
+          <p>
+            <Link href={"/what-is-capoeira"}>Learn more!</Link>
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
